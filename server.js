017febc57a441189ecb6c1be7542631c1f740ae7
Sboakye-uofsc <SBOAKYE@email.sc.edu>
2025-11-09 15:56:59 -0500

    ready to deploy

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(express.static("public"));
-app.use(cors());
+const cors = require("cors");
+app.use(cors({
+  origin: ['https://sboakye-uofsc.github.io/statistics-server/', 'http://localhost:3000/statistics-server'] // add your domains
+}));
 
 app.get('/',(request,respond) => {
 	respond.sendFile(__dirname + "/index.html");
@@ -35,6 +38,58 @@ app.get("/api/GameData", (request,respond) => {
 	respond.send(data);
 });
 
-app.listen(3001, () => {
-	console.log("Server Backend is up");
+app.get("/api/Review", (request,respond) => {
+	const reviewData = [
+		{
+			"_id": 1,
+			"name_game": "Peak",
+			"released": "2025-16-06",
+			"img": "./myjson/img/peak.png",
+			"username": "Unicakes753",
+			"date_respond": "2025-20-09",
+			"description": "Ya'll I really have a obsession with this game",
+			"rating": 5,
+			"reviews":["Since the first time I play this time, I fell in love with it"]
+		},
+		{
+			"_id": 2,
+			"name_game": "Castle Crashers",
+			"released": "2012-26-09",
+			"img": "./myjson/img/castle.png",
+			"username": "YOYLL347",
+			"date_respond": "2024-19-05",
+			"description": "This bring me back to the good old days",
+			"rating": 4.5,
+			"reviews":["I remember when this game first came out on my birshday and my mom got it for me as a gift"]
+		},
+		{
+			"_id": 3,
+			"name_game": "Runescape Dragonwilds",
+			"released": "2025-15-04",
+			"img": "./myjson/img/dragon.png",
+			"username": "Maical_Moooon",
+			"date_respond": "2025-15-04",
+			"description": "The game is not too bad, I will definitely play with friends",
+			"rating": 4,
+			"reviews":["The game is cool. Really like the combat in this game"]
+		},
+		{
+			"_id": 4,
+			"name_game": "Marvel Rivals",
+			"released": "2024-05-12",
+			"img": "./myjson/img/rivals.png",
+			"username": "Rivals_Katch",
+			"date_respond": "2024-09-12",
+			"description": "I will definitely play with friends",
+			"rating": 4.9,
+			"reviews":["The game is cool. Really like the combat in this game"]
+		}
+	];
+	respond.send(reviewData);
+});
+
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
